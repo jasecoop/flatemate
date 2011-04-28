ec473995e045dce29cf1636737ea074db3830c1c
jasecoop <jase.coop@gmail.com>
2011-04-28 16:35:59 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flatmate::Application.config.secret_token = '51e68c7d2f701b766d1fd670b2ec847dc68a63403c1775e263be72d3c000c34f45691aabb21fc66f6e7a8bc1956f647067113406e6f8d86fe09107a21fb9a7e4'
